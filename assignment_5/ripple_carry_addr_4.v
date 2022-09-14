fdf4480425fce4b67a881ce24205313514f6272a
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-09-14 14:18:00 +0530

    Refactor 4 bit rca

--- assignment_5/ripple_carry_addr_4.v
+++ assignment_5/ripple_carry_addr_4.v
@@ -9,22 +9,6 @@
 /////////////////////////////////////////////
 */
 
-// half adder
-// inputs: in1, in2, cin (cin is input carry)
-// outputs: out, cout (out is sum, cout is output carry)
-
-module half_addr(
-    input in1,
-    input in2,
-    output out,
-    output cout
-    );
-
-    assign out = in1 ^ in2;
-    assign cout = in1 & in2;
-
-endmodule
-
 // full adder
 // inputs: in1, in2, cin (cin is input carry)
 // outputs: out, cout (out is sum, cout is output carry)
@@ -37,14 +21,23 @@ module full_addr(
     output cout
     );
 
-    wire sum1;       // output sum of 1 st half adder
-    wire carry1;     // output carry of 1st half adder
-    wire carry2;     // output carry of 2nd half adder 
+    wire w1, w2;
+    wire v1, v2, v3, v4;
 
-    half_addr h1(in1,in2,sum1,carry1);
-    half_addr h2(sum1,cin,out,carry2);
+    // Structural design pattern
 
-    xor(cout, carry1, carry2);
+    // out = in1 ^ in2 ^ cin
+    xor(w1, in1, in2);
+    xor(out, w1, cin);
+
+    // cout = (in1 & in2) | (in1 & cin) | (in2 & cin)
+    and(v1, in1, in2);
+    and(v2, in1, cin);
+    and(v3, in2, cin);
+
+    or(v4, v1, v2);
+    or(cout, v4, v3);
+
 endmodule
 
 // ripple carry adder (4 bit)
@@ -53,6 +46,7 @@ endmodule
 
 module ripple_carry_addr_4(
     input[3:0] in1, 
+    input[3:0] in2,
     input cin, 
     output[3:0] out,
     output cout
@@ -64,10 +58,10 @@ module ripple_carry_addr_4(
     // concatenating 4 full adders to make a ripple carry adder for 4 bits
     // connect output of previous full adder with input of current full adder using wires
     // Structural design pattern
-    full_addr addr1(.in1(in1[0]),.in2(1'b1),.cin(cin),.out(out[0]),.cout(w1));
-    full_addr addr2(.in1(in1[1]),.in2(1'b0),.cin(w1),.out(out[1]),.cout(w2));    
-    full_addr addr3(.in1(in1[2]),.in2(1'b0),.cin(w2),.out(out[2]),.cout(w3));
-    full_addr addr4(.in1(in1[3]),.in2(1'b0),.cin(w3),.out(out[3]),.cout(cout));
+    full_addr addr1(.in1(in1[0]),.in2(in2[0]),.cin(cin),.out(out[0]),.cout(w1));
+    full_addr addr2(.in1(in1[1]),.in2(in2[1]),.cin(w1),.out(out[1]),.cout(w2));    
+    full_addr addr3(.in1(in1[2]),.in2(in2[2]),.cin(w2),.out(out[2]),.cout(w3));
+    full_addr addr4(.in1(in1[3]),.in2(in2[3]),.cin(w3),.out(out[3]),.cout(cout));
 
 endmodule
 
