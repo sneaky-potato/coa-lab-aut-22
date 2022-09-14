fdfff8ac4976b2fc580c23c7e0f9406f0937ddf7
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-09-14 15:10:46 +0530

    Replace rca by cla adder in testbench

--- assignment_5/binary_cnt_struct.v
+++ assignment_5/binary_cnt_struct.v
@@ -20,17 +20,15 @@ module binary_cnt_struct(input clk, input rst, output wire [3:0] cnt);
 
     // pass in values
     // in1 <- cnt
-    // in2 <- 0001
-    // cin <- 0
+    // cin <- 1
     // s_out <- out
     // cout <- cout
-    ripple_carry_addr_4 rca_4(.in1(cnt), .in2(4'd0), .cin(1'b1), .out(s_out), .cout(cout));
+    add_by_one_carry add1(.in1(cnt), .cin(1'b1), .out(s_out), .cout(cout));
 
     // use the dff to assign bit values of the counter sum (s_out) to original counter (cnt) 
     dff_struct d1(s_out[0], clk, rst, cnt[0]);
     dff_struct d2(s_out[1], clk, rst, cnt[1]);
     dff_struct d3(s_out[2], clk, rst, cnt[2]);
     dff_struct d4(s_out[3], clk, rst, cnt[3]);
-
 
 endmodule
