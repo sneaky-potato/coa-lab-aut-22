bcb40128841142d55dd38e390e7db5552258b948
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-09-15 09:47:31 +0530

    Refactor binary counter testbench

--- assignment_5/binary_cnt_tb.v
+++ assignment_5/binary_cnt_tb.v
@@ -3,12 +3,13 @@
 module binary_cnt_tb;
 
     reg clk,rst;
-    reg [3:0] data;
     wire [3:0] cnt;
 
     binary_cnt counter(.clk(clk), .rst(rst), .cnt(cnt));
     
     // block for clock
+    // This clock will flip its value every 3ns  
+
     initial 
     begin 
         clk = 1'b0; 
@@ -18,15 +19,17 @@ module binary_cnt_tb;
     // block for reset input wire
     initial 
     begin 
-        rst=1'b1;
-        #7 rst=1'b0; 
-        #35 rst=1'b1;
+        rst=1'b0;
+        #7;
+        rst=1'b1; 
+        #35;
+        rst=1'b0;
     end
 
     // block for printing on monitor
     initial 
     begin 
-        $monitor("time=%0d, rst=%b, count=%d",$time,rst,cnt);
+        $monitor("time=%0d, rst=%b, count=%b",$time,rst,cnt);
     end
     
 endmodule
